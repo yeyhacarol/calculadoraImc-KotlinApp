ee3e62eb89661d67685988039a138960d42e4351
CAROLINA VITÓRIA CHAVES SILVA <21276093@CFP127ALUNO.LOCAL>
2022-02-15 14:49:04 -0300

    alterando a visualização do resultado para decimal ##.##

--- app/src/main/java/com/senai/calculadora_imc/MainActivity.kt
+++ app/src/main/java/com/senai/calculadora_imc/MainActivity.kt
@@ -1,11 +1,11 @@
 package com.senai.calculadora_imc
 
-import android.icu.text.DecimalFormat
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import java.text.DecimalFormat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,6 +13,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val calcular = findViewById<Button>(R.id.calcular)
+        val decimal = DecimalFormat("#.##")
 
         calcular.setOnClickListener() {
             val peso = findViewById<EditText>(R.id.peso).text.toString().toDouble()
@@ -21,17 +22,17 @@ class MainActivity : AppCompatActivity() {
             val resultado = findViewById<TextView>(R.id.resultado)
 
             if (imc < 18.5) {
-                resultado.text = "Seu imc é ${imc} e você está abaixo do peso"
+                resultado.text = "Seu imc é ${decimal.format(imc)} e você está abaixo do peso"
             } else if (imc > 18.5 && imc < 24.9) {
-                resultado.text = "Seu imc é ${imc} e você está no peso ideal"
+                resultado.text = "Seu imc é ${decimal.format(imc)} e você está no peso ideal"
             } else if (imc > 25 && imc < 29.9) {
-                resultado.text = "Seu imc é ${imc} e você está acima do peso"
+                resultado.text = "Seu imc é ${decimal.format(imc)} e você está acima do peso"
             } else if (imc > 30 && imc < 34.9) {
-                resultado.text = "Seu imc é ${imc} e você está obeso"
+                resultado.text = "Seu imc é ${decimal.format(imc)} e você está obeso"
             } else if (imc > 35 && imc < 39.9) {
-                resultado.text = "Seu imc é ${imc} e você está obeso grau II"
+                resultado.text = "Seu imc é ${decimal.format(imc)} e você está obeso grau II"
             } else {
-                resultado.text = "Seu imc é ${imc} e você está com obesidade mórbida"
+                resultado.text = "Seu imc é ${decimal.format(imc)} e você está com obesidade mórbida"
             }
 
         }
