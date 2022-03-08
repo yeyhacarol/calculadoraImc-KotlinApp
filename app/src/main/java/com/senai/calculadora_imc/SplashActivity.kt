a947f9ac5e4325d3973b41531278f90fd4d82449
CAROLINA VITÓRIA CHAVES SILVA <21276093@CFP127ALUNO.LOCAL>
2022-03-08 14:50:22 -0300

    criando splash e alterando imagem de ícone

---
+++ app/src/main/java/com/senai/calculadora_imc/SplashActivity.kt
@@ -0,0 +1,24 @@
+package com.senai.calculadora_imc
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import java.util.*
+import kotlin.concurrent.schedule
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        Timer().schedule(3000) {abrirMain()}
+    }
+
+    private fun abrirMain() {
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+
+
+}
